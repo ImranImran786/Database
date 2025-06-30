823d568c4959b43fcb231bdea0fe45f218086dee
ImranImran786 <168235032+ImranImran786@users.noreply.github.com>
2025-06-30 10:15:53 +0500

    eight

--- server.js
+++ server.js
@@ -273,9 +273,9 @@ const io = new Server(httpServer, {
       "http://localhost:3001",
       "http://localhost:3002"
 
-        "https://admin-frontend-alpha-olive.vercel.app/login",
-    "https://client-frontend-eta.vercel.app/login",
-    "https://driver-frontend-zeta.vercel.app/login"
+    //     "https://admin-frontend-alpha-olive.vercel.app/",
+    // "https://client-frontend-eta.vercel.app/",
+    // "https://driver-frontend-zeta.vercel.app/"
     ],
     methods: ["GET", "POST"]
   }
@@ -288,9 +288,9 @@ app.use(cors({
     "http://localhost:3001",
     "http://localhost:3002",
 
-    "https://admin-frontend-alpha-olive.vercel.app/login",
-    "https://client-frontend-eta.vercel.app/login",
-    "https://driver-frontend-zeta.vercel.app/login"
+    "https://admin-frontend-alpha-olive.vercel.app/",
+    "https://client-frontend-eta.vercel.app/",
+    "https://driver-frontend-zeta.vercel.app/"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
